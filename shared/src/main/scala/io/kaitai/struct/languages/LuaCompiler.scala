622ce800a10c0d95f72dd5880580406d7284e067
Milan Hauth <milahu@gmail.com>
2025-07-26 16:41:47 +0200

    fixup LuaCompiler.scala
    fix: not found: value expected

--- shared/src/main/scala/io/kaitai/struct/languages/LuaCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/LuaCompiler.scala
@@ -445,18 +445,34 @@ class LuaCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     checkExpr: Ast.expr,
     err: KSError,
     errArgs: List[Ast.expr],
+    // TODO fix merge? use parameter "useIo"?
     useIo: Boolean,
     // expected: Option[Ast.expr] = None
   ): Unit = {
     val actualStr = expression(Ast.expr.InternalName(attr.id))
     out.puts(s"if not(${translator.translate(checkExpr)}) then")
     out.inc
+    // error: not found: value expected
+    /*
     val msg = err match {
       case _: ValidationNotEqualError => {
         val expectedStr = expected.get
         s""""not equal, expected " .. $expectedStr .. ", but got " .. $actualStr"""
       }
       case _ => expression(Ast.expr.Str(ksErrorName(err)))
+    }
+    */
+    // based on "def attrValidate":
+    val errArgsCode = errArgs.map(translator.translate)
+    val msg = err match {
+      case _: ValidationNotEqualError => {
+        val (expected, actual) = (
+          errArgsCode.lift(0).getOrElse("[expected]"),
+          errArgsCode.lift(1).getOrElse("[actual]")
+        )
+        s""""not equal, expected " ..  $expected .. ", but got " .. $actual"""
+      }
+      case _ => "\"" + ksErrorName(err) + "\""
     }
     out.puts(s"error($msg)")
     out.dec
