9d77fe77666d25dd798bcb6f81ff6f05f4cbe105
Milan Hauth <milahu@gmail.com>
2025-07-26 16:43:36 +0200

    fixup PHPCompiler.scala
    fix: method attrValidateExpr overrides nothing

--- shared/src/main/scala/io/kaitai/struct/languages/PHPCompiler.scala
+++ shared/src/main/scala/io/kaitai/struct/languages/PHPCompiler.scala
@@ -525,10 +525,12 @@ class PHPCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     attr: AttrLikeSpec,
     checkExpr: Ast.expr,
     err: KSError,
+    errArgs: List[Ast.expr],
     useIo: Boolean,
-    expected: Option[Ast.expr] = None
+    // expected: Option[Ast.expr] = None
   ): Unit = {
-    val errArgsStr = expected.map(expression) ++ List(
+    // val errArgsStr = expected.map(expression) ++ List(
+    val errArgsStr = List(
       expression(Ast.expr.InternalName(attr.id)),
       if (useIo) expression(Ast.expr.InternalName(IoIdentifier)) else "null",
       expression(Ast.expr.Str(attr.path.mkString("/", "/", "")))
@@ -545,7 +547,8 @@ class PHPCompiler(typeProvider: ClassTypeProvider, config: RuntimeConfig)
     et: EnumType,
     valueExpr: Ast.expr,
     err: ValidationNotInEnumError,
-    errArgs: List[Ast.expr]
+    errArgs: List[Ast.expr],
+    useIo: Boolean,
   ): Unit = {
     val enumSpec = et.enumSpec.get
     val enumRef = translator.types2classAbs(enumSpec.name)
